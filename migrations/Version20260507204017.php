5ae8d2800f49938aa430d22fda3ab5d3f2ef2389
Rim Slama <reemslama21@gmail.com>
2026-05-07 23:35:40 +0200

    nouveau connexion

--- migrations/Version20260507204017.php
+++ migrations/Version20260507204017.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20260501215137 extends AbstractMigration
+final class Version20260507204017 extends AbstractMigration
 {
     public function getDescription(): string
     {
